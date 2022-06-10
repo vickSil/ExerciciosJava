276972468a4bdf256e84f41b209d0a3a757f6550
Vitória Silva <102378306+vickSil@users.noreply.github.com>
2022-06-10 10:48:30 -0300

    Update ClienteBean.java

--- PedidoRelacionamentoJPA/src/main/java/br/com/rafaelcosta/application/ejb/ClienteBean.java
+++ PedidoRelacionamentoJPA/src/main/java/br/com/rafaelcosta/application/ejb/ClienteBean.java
@@ -11,10 +11,11 @@ import br.com.rafaelcosta.application.model.Cliente;
 
 @Stateless
 public class ClienteBean {
-
+	//guarda as entidades que estão sendo usadas com o entity manager
 	@PersistenceContext
+	//provê um meio de criar consultar,criar e buscar objetos do banco de dados
 	private EntityManager em;
-	
+	//quando conectado com o banco dedaos, damos comandos mysql para executar no código 
 	public List<Cliente> listar() {
 		return em.createQuery("SELECT c FROM Cliente c", Cliente.class).getResultList();
 	}
