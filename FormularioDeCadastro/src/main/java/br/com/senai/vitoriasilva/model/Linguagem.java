bf332e18cc7ff13e03130c60b2668c550ac26b43
Vitória Silva <102378306+vickSil@users.noreply.github.com>
2022-06-10 09:31:18 -0300

    Update Linguagem.java

--- FormularioDeCadastro/src/main/java/br/com/senai/vitoriasilva/model/Linguagem.java
+++ FormularioDeCadastro/src/main/java/br/com/senai/vitoriasilva/model/Linguagem.java
@@ -4,19 +4,20 @@ public class Linguagem {
 
 	private Integer id;
 	private String nome;
-	
+	/* constante linguagem */
 	public static final Linguagem[] LINGUAGENS;
 	
 	static {
+		/* reservando espaço para inserir as linguagens */
 		LINGUAGENS = new Linguagem[5];
-		
+		/* inserindo linguagens que mais tarde serão listadas */
 		LINGUAGENS[0] = new Linguagem(1, "Java");
 		LINGUAGENS[1] = new Linguagem(2, "IOS");
 		LINGUAGENS[2] = new Linguagem(3, "C++");
 		LINGUAGENS[3] = new Linguagem(4, "C#");
 		LINGUAGENS[4] = new Linguagem(5, "PHP");
 	}
-	
+	/* atribuiçõ de valores e id ao campo linguagem */
 	public Linguagem(Integer id, String nome) {
 		this.setId(id);
 		this.setNome(nome);
