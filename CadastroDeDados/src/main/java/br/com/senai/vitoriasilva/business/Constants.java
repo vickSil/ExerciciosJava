8a8dd3366cf4c49f58aa4cb702111d9a0668891a
Vitória Silva <102378306+vickSil@users.noreply.github.com>
2022-06-10 09:45:36 -0300

    Update Constants.java

--- CadastroDeDados/src/main/java/br/com/senai/vitoriasilva/business/Constants.java
+++ CadastroDeDados/src/main/java/br/com/senai/vitoriasilva/business/Constants.java
@@ -8,32 +8,35 @@ import br.com.senai.vitoriasilva.model.Interesse;
 
 public class Constants {
 
+/* criando constantes "ESTADOS" e "INTERESSES" */
 	public static final List<Estado> ESTADOS;
 	public static final List<Interesse> INTERESSES;	
 	
 	
 	//static, roda uma vez preenchendo os campos 
 	static {
+		/* criando e preenchendo a lista com os estados e siglas */
 		ESTADOS = new ArrayList<>();
 		ESTADOS.add(new Estado("Acre","AC"));
 		ESTADOS.add(new Estado("Alagoas","AL"));
 		ESTADOS.add(new Estado("Amazonas","AL"));
 		ESTADOS.add(new Estado("Bahia","BA"));
-		ESTADOS.add(new Estado("Ceará","CE"));
-		ESTADOS.add(new Estado("Espírito Santo","ES"));
+		ESTADOS.add(new Estado("CearÃ¡","CE"));
+		ESTADOS.add(new Estado("EspÃ­rito Santo","ES"));
 		ESTADOS.add(new Estado("Minas Gerais","MG"));
-		ESTADOS.add(new Estado("Paraná","PR"));
+		ESTADOS.add(new Estado("ParanÃ¡","PR"));
 		ESTADOS.add(new Estado("Rio de Janeiro","RJ"));
 		ESTADOS.add(new Estado("Santa Catarina","SC"));
-		ESTADOS.add(new Estado("São Paulo","SP"));
+		ESTADOS.add(new Estado("SÃ£o Paulo","SP"));
 		
+		/* criando e preenchendo a lista de interesses */
 		INTERESSES = new ArrayList<>();
 		INTERESSES.add(new Interesse(1 , "Esportes"));
-		INTERESSES.add(new Interesse(2 , "Música"));
+		INTERESSES.add(new Interesse(2 , "MÃºsica"));
 		INTERESSES.add(new Interesse(3 , "Artes Marciais"));
 		INTERESSES.add(new Interesse(4 , "Viagens"));
 		INTERESSES.add(new Interesse(5 , "Cinema"));
-		INTERESSES.add(new Interesse(6 , "Dança"));
+		INTERESSES.add(new Interesse(6 , "DanÃ§a"));
 		
 	}
 }
